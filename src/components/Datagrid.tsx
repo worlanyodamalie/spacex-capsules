c9a09d66368fcbcc188df8e21719184375f7710c
Worlanyo <worladamalie@gmail.com>
2023-07-23 14:34:38 +0000

    Implement capsule filter functionality

--- src/components/Datagrid.tsx
+++ src/components/Datagrid.tsx
@@ -1,15 +1,43 @@
 import  { useState,useEffect } from "react";
+import Datepicker from "react-tailwindcss-datepicker"; 
+import { DatepickerType , DateValueType } from "react-tailwindcss-datepicker/dist/types";
+
 import Modal from "./Modal";
+
+type capsuleType = {
+    docs: [{
+        id:string,
+        reuse_count:number,
+        water_landings:number,
+        land_landings:number,
+        last_update:string,
+        serial:string,
+        status:string,
+        type:string,
+        launches: {links: {flickr: {original: string[] }} , details: string | null ,date_local: string , name: string}[]
+    }],
+    totalDocs: number,
+    totalPages: number,
+    page: number,
+    hasNextPage: boolean,
+    hasPrevPage: boolean
+    
+    //launches:[]{name:string}
+}
+
+type launchType = {links: {flickr: {original: string[] }} , details: string | null ,date_local: string , name: string}[]
+
 
 export function DataGrid(){
     const tableHeader = ['Type' , 'Serial' ,  'Water landings' , 'Land landings' , 'Last update', ''];
-    
     const launchTableHeader = ['Name','Date ' , 'Details']
+    const url = "http://127.0.0.1:8000/capsules"; 
+    const status = [ "active" , "destroyed", "retired",  "unknown"]
+    const types = [ "Dragon 1.0" , "Dragon 1.1" , "Dragon 2.0"]
 
-    const url = "http://127.0.0.1:8000/capsules";
-    
-    const [count, setCount] = useState(1)
-
+    const [data , setData] = useState<capsuleType>()
+    const [launch , setLauch] = useState<launchType>([])
+    const [isOpen,setIsOpen] = useState(false)
     const [query,setQuery] = useState({
         "query": {},
         "options": {
@@ -17,6 +45,14 @@ export function DataGrid(){
             page: 1
         }
     })
+    const [value, setValue] = useState({ 
+        startDate: new Date(), 
+        endDate: new Date().setMonth(12)
+        }); 
+
+    const handleValueChange = (newValue: DatepickerType["value"] | null) => {
+        setValue(newValue); 
+        }    
     
     const previousPage = () => {
         setQuery(
@@ -31,7 +67,7 @@ export function DataGrid(){
              })
          )
     
- }
+     }
    
 
     const nextPage = () => {
@@ -49,37 +85,41 @@ export function DataGrid(){
        
     }
 
-    type capsuleType = {
-        docs: [{
-            id:string,
-            reuse_count:number,
-            water_landings:number,
-            land_landings:number,
-            last_update:string,
-            serial:string,
-            status:string,
-            type:string,
-            launches: {links: {flickr: {original: string[] }} , details: string | null ,date_local: string , name: string}[]
-        }],
-        totalDocs: number,
-        totalPages: number,
-        page: number,
-        hasNextPage: boolean,
-        hasPrevPage: boolean
-        
-        //launches:[]{name:string}
-    }
-
-    type launchType = {links: {flickr: {original: string[] }} , details: string | null ,date_local: string , name: string}[]
-
-    const [data , setData] = useState<capsuleType>()
-    const [launch , setLauch] = useState<launchType>([])
     
-    const [isOpen,setIsOpen] = useState(false)
-
     const lauchModal = (launch: launchType) => {
         setLauch(launch)
         setIsOpen(true)
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        // console.log("newValue:", value); 
+        // console.log(event.target.type.value)
+        // console.log(event.target.status.value)
+        setQuery(
+            prevState => ({
+                "query": {
+                    ...prevState.query,
+                    "date_utc": {
+                        "$gte": new Date(value.endDate).toISOString(),
+                        "$lte": new Date(value.startDate).toISOString() 
+                     },
+                     "$or": [
+                        {
+                            "status": event.target.status.value
+                        },
+                        {
+                            "type": event.target.type.value
+                        }
+                    ]
+                },
+                "options": {
+                    ...prevState.options,
+                   
+                }
+            })
+        )
     }
    
     useEffect(() => {
@@ -114,30 +154,75 @@ export function DataGrid(){
           {" "}
           Search for capsule information
         </p>
-        <form className="flex flex-row gap-3 mb-6 justify-center">
-          <div className="">
-            <input
-              type="text"
-              id="default-input"
-              className="font-sora font-normal border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
+        <form onSubmit={handleSubmit} className="flex flex-row gap-6 mb-6 justify-center">
+          <div className="date--range-filter">
+            <Datepicker 
+               value={value} 
+               onChange={handleValueChange} 
+               className="border border-gray-300"
+               />
+          </div>
+          <div >
+            <select
+              name="type"
+              className="font-sora bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option
+                selected
+                className="font-sora font-normal"
+                value=""
+                disabled
+              >
+                Select capsule type{" "}
+              </option>
+
+              {types.map((item) => {
+                return (
+                  <option
+                    key={item}
+                    className="font-sora font-normal"
+                    value={item}
+                  >
+                    {item}
+                  </option>
+                );
+              })}
+            </select>
           </div>
           <div>
             <select
-              id="countries"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              id="status"
+              name="status"
+              className="font-sora bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option value="US">United States</option>
-              <option value="CA">Canada</option>
-              <option value="FR">France</option>
-              <option value="DE">Germany</option>
+              <option
+                selected
+                className="font-sora font-normal"
+                value=""
+                disabled
+              >
+                Select status{" "}
+              </option>
+
+              {status.map((item) => {
+                return (
+                  <option
+                    key={item}
+                    className="font-sora font-normal"
+                    value={item}
+                  >
+                    {item}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <button
-            type="button"
+            type="submit"
+            // className="px-3 py-2 text-sm font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             className="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
           >
-            <svg
+            {/* <svg
               aria-hidden="true"
               role="status"
               className="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600"
@@ -153,8 +238,8 @@ export function DataGrid(){
                 d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                 fill="#1C64F2"
               />
-            </svg>
-            Loading...
+            </svg> */}
+            Search
           </button>
         </form>
         <div>
@@ -232,13 +317,12 @@ export function DataGrid(){
                 </span>
               </span>
               <div className="inline-flex mt-2 xs:mt-0">
-                <button 
-                    className="font-sora flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 rounded-none hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"   
-                    onClick={previousPage}
+                <button
+                  className="font-sora flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 rounded-none hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={previousPage}
                   // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
                   disabled={data?.hasPrevPage! ? false : true}
                   //className="cursor-pointer flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-
                 >
                   <svg
                     className="w-3.5 h-3.5 mr-2"
@@ -257,12 +341,12 @@ export function DataGrid(){
                   </svg>
                   Prev
                 </button>
-                <button 
-                 //className="cursor-pointer flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                 className="font-sora flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-none hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                 onClick={nextPage}
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-                disabled={data?.hasNextPage! ? false : true}
+                <button
+                  //className="cursor-pointer flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  className="font-sora flex items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-none hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  onClick={nextPage}
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+                  disabled={data?.hasNextPage! ? false : true}
                 >
                   Next
                   <svg
